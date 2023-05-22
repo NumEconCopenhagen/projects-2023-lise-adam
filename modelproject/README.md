766aebbd5eaee71ef714b6e176074bbb5973aaac
fls808 <124403474+fls808@users.noreply.github.com>
2023-05-22 18:30:27 +0200

    Add files via upload

--- modelproject/README.md
+++ modelproject/README.md
@@ -1,6 +1,9 @@
 # Model analysis project
 
-Our project is titled **PROJECT TITLE** and is about EXPLAIN.
+Our project is titled **Should you go partying or not? - A Principal Agent model ** and is about a principal who hires an agent.
+The agent can choose to go partying or staying home and his probability of high sales the day after depends on this choice. The principal decides a potentially performance-based wage contract and should decide what wages are optimal. 
+We solve the model analytically and numerically and compare these solutions. Futhermore we illustrate the principal's optimal choice for different parameter values.
+Lastly, we extend the model with letting the principal choose whether to monitor the agent or not and having to pay a cost if he/she decides to do so. We illustrate how the optimal choice depends on monitoring cost. 
 
 The **results** of the project can be seen from running [modelproject.ipynb](modelproject.ipynb).
 
--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -8,7 +8,7 @@ plt.rcParams.update({'font.size': 14})
 
 class PrincipalAgentClass():
 
-    def __init__(self, do_print=True):
+    def __init__(self, do_print=False):
         """  setup the model """
 
         if do_print: print('initializing the model:')
@@ -18,40 +18,35 @@ class PrincipalAgentClass():
         sol = self.sol = SimpleNamespace()
 
         # b. parameters
-        par.pi_0 = 0.3  # probability of high sales when employee is hungover
-        par.c = 1.0  # employee's cost of working hard/Not going out
+        par.pi_0 = 0.4  # probability of high sales when employee is hungover
+        par.pi_1 = 0.7  # probability of high sales when employee is not hungover
+        par.c = 1.0  # employee's cost of providing hard effort/not going out
         par.ubar = 5.0  # reservation utility level
         par.rho = 0.5 # CRRA Parameter
-        par.e = 1.0 # Level of effort (= 1 when not hungover, =0 when hungover)
+        par.M = 0.3 # cost of monitoring the agent 
+        par.highsales = 200 # sales when high
+        par.lowsales = 50 # sales when low
 
-        par.highsales = 100
-        par.lowsales = 0
-
-        par.pi_1 = 0.7  # probability of high sales when employee is not hungover
-        
         # c. solution 
         shape = np.zeros([1,1])
 
-        sol.wH_high_effort = shape + np.nan
-        sol.wL_high_effort = shape + np.nan
-        sol.profit_high_effort = shape + np.nan
+        sol.wH_high_effort = shape + np.nan # optimal wH, the agent works hard 
+        sol.wL_high_effort = shape + np.nan # optimal wL, the agent works hard 
+        sol.profit_high_effort = shape + np.nan # resulting profit, the agent works hard 
 
-        sol.wH_loweffort = shape + np.nan
-        sol.wL_loweffort = shape + np.nan
-        sol.profit_low_effort = shape + np.nan
+        sol.wH_loweffort = shape + np.nan # optimal wH, the agent does not work hard 
+        sol.wL_loweffort = shape + np.nan # optimal wL,the agent does not work hard 
+        sol.profit_low_effort = shape + np.nan # resulting profit, the agent does not work hard 
 
-        sol.wH = shape + np.nan
-        sol.wL = shape + np.nan
-        sol.high_effort = shape + np.nan
-        sol.profit = shape + np.nan
-
-        # d. illustrate, compare
-        par.steps = 20
-
+        sol.wH = shape + np.nan # optimal wH (can both be wH_high_effort or wH_loweffort)
+        sol.wL = shape + np.nan # optimal wL (can both be wL_high_effort or wL_loweffort)
+        sol.high_effort = shape + np.nan # dummy for optimal to "make" the agent work hard
+        sol.profit = shape + np.nan # highest profit achievable 
 
     def high_sales_prob(self, e):
-        """ expected sales given the employee's effort level """
+        """ probability of high sales given the employee's effort level """
 
+        # a. unpack
         par = self.par
         if e == 0:
             return par.pi_0
@@ -59,9 +54,8 @@ class PrincipalAgentClass():
             return par.pi_1
     
     def expected_wage(self, wH, wL,e):
-        """ calculate the expected wage and the utility of the employee given the wage contract, effort level and probability of high sales"""
-
-        par = self.par
+        """ calculate the expected wage of the employee given the wage contract\
+            and effort level """
 
         if e==0:
             wage = self.high_sales_prob(0) * wH + (1 - self.high_sales_prob(0)) * wL
@@ -70,33 +64,75 @@ class PrincipalAgentClass():
         
         return wage
     
-    def expected_profit(self, wH, wL, e):
-        
+    def expected_profit(self, wH, wL, e, monitoring):
+        """ calculate the expected proft of the principal given the wage contract,\
+            effort level and monitoring decision """
+
+        # a. unpack
         par = self.par 
         if e == 0:
-            expect_profit = self.high_sales_prob(0)*par.highsales + (1-self.high_sales_prob(0))*par.lowsales - self.expected_wage(wH, wL,0)
+            expect_profit = \
+                self.high_sales_prob(0)*par.highsales \
+                + (1-self.high_sales_prob(0))*par.lowsales \
+                - self.expected_wage(wH, wL,0) - par.M*monitoring
         else: 
-            expect_profit = self.high_sales_prob(1)*par.highsales + (1-self.high_sales_prob(1))*par.lowsales - self.expected_wage(wH, wL,1)
+            expect_profit = \
+                self.high_sales_prob(1)*par.highsales \
+                + (1-self.high_sales_prob(1))*par.lowsales \
+                - self.expected_wage(wH, wL,1) - par.M*monitoring
         
         return expect_profit
 
     def calc_utility(self,wH,wL,e):
-        par = self.par
-        utility = self.high_sales_prob(e) *( wH**(1-par.rho) /(1-par.rho) - par.c*e)+(1-self.high_sales_prob(e))*(wL**(1-par.rho)/(1-par.rho) -par.c*e)
-        return utility
-
-    def solve_high_effort(self):
-        """ Solving model """
-
+        """ calculate the expected utility of the agent given the wage contract,\
+            effort level """
+        
         # a. unpack
         par = self.par
+        utility = \
+            self.high_sales_prob(e) *( wH**(1-par.rho) /(1-par.rho) - par.c*e) \
+            + (1-self.high_sales_prob(e))*(wL**(1-par.rho)/(1-par.rho) -par.c*e)
+        
+        return utility
+    
+    def IR_constraint(self,wH,wL,e):
+        """ calculate the IR constraint given the wage contract and effort level """
+       
+        # a. unpack
+        par = self.par
+
+        if e==0:
+            IR = self.calc_utility(wH,wL,0)-par.ubar
+        else:
+            IR = self.calc_utility(wH,wL,1)-par.ubar
+        return IR 
+    
+    def IC_constraint(self,wH,wL,high_effort):
+        """ calculate the IC constraints given the wage contract"""
+
+        if high_effort==0:
+            IC = self.calc_utility(wH,wL,0)-self.calc_utility(wH,wL,1)
+        else:
+            IC = self.calc_utility(wH,wL,1)-self.calc_utility(wH,wL,0)
+
+        return IC 
+        
+
+    def solve_high_effort(self, monitoring=0):
+        """ solving model when the principal wants high effort given monitoring decision """
+
+        # a. unpack
         sol = self.sol
 
         # b. objective function to be minimized
         obj = lambda x: self.expected_wage(x[0],x[1],1)
 
-        # c. Setting constraints
-        cons = [ {'type': 'ineq', 'fun': lambda x:  self.calc_utility(x[0],x[1],1)-par.ubar}, {'type': 'ineq', 'fun': lambda x: self.calc_utility(x[0],x[1],1)-self.calc_utility(x[0],x[1],0)}]
+        # c. setting constraints
+        if monitoring == 0:
+            cons = [ {'type': 'ineq', 'fun': lambda x:  self.IR_constraint(x[0],x[1],1)}, \
+                     {'type': 'ineq', 'fun': lambda x: self.IC_constraint(x[0],x[1],1)}]
+        else: 
+            cons = [ {'type': 'ineq', 'fun': lambda x:  self.IR_constraint(x[0],x[1],1)}]
 
         # d. setting bounds 
         bounds = ((0,100),(0,100))
@@ -105,7 +141,8 @@ class PrincipalAgentClass():
         initial_guess = [12.25,2.25] 
 
         # e. call optimizer
-        res_high = optimize.minimize(obj, initial_guess, constraints=cons, bounds = bounds, method='SLSQP', tol=1e-12)
+        res_high = optimize.minimize(obj, initial_guess, constraints=cons, \
+                                     bounds = bounds, method='SLSQP', tol=1e-15)
 
         # f. store results
         sol.wH_high_effort = res_high.x[0]
@@ -113,18 +150,21 @@ class PrincipalAgentClass():
        
         return res_high
     
-    def solve_low_effort(self):
-        """ Solving model """
+    def solve_low_effort(self, monitoring=0):
+        """ solving model when the principal wants high effort given monitoring decision """
 
         # a. unpack
-        par = self.par
         sol = self.sol
 
         # b. objective function to be minimized
         obj = lambda x: self.expected_wage(x[0],x[1],0)
 
-        # c. Setting constraints
-        cons = [ {'type': 'ineq', 'fun': lambda x:  self.calc_utility(x[0],x[1],0)-par.ubar}, {'type': 'ineq', 'fun': lambda x: self.calc_utility(x[0],x[1],0)-self.calc_utility(x[0],x[1],1)}]
+        # c. setting constraints
+        if monitoring == 0:
+            cons = [ {'type': 'ineq', 'fun': lambda x:  self.IR_constraint(x[0],x[1],0)}, \
+                     {'type': 'ineq', 'fun': lambda x: self.IC_constraint(x[0],x[1],0)}]
+        else: 
+            cons = [ {'type': 'ineq', 'fun': lambda x:  self.IR_constraint(x[0],x[1],0)}]
 
         # d. setting bounds 
         bounds = ((0,100),(0,100))
@@ -133,7 +173,8 @@ class PrincipalAgentClass():
         initial_guess = [12.25,2.25] 
 
         # e. call optimizer
-        res_low = optimize.minimize(obj, initial_guess, constraints=cons, bounds = bounds, method='SLSQP', tol=1e-12)
+        res_low = optimize.minimize(obj, initial_guess, constraints=cons, \
+                                    bounds = bounds, method='SLSQP', tol=1e-15)
 
         # f. store results
         sol.wH_loweffort = res_low.x[0]
@@ -141,16 +182,21 @@ class PrincipalAgentClass():
        
         return res_low
     
-    def solve(self):
+    def solve(self, monitoring = 0):
+        """ solving model """
+        
         # a. unpack
         sol = self.sol
 
-        self.solve_high_effort()
-        sol.profit_high_effort = self.expected_profit(sol.wH_high_effort, sol.wL_high_effort, 1)
+        # b. calculate profit when the principal wants high effort given monitoring decision
+        self.solve_high_effort(monitoring)
+        sol.profit_high_effort = self.expected_profit(sol.wH_high_effort,sol.wL_high_effort,1,monitoring)
 
-        self.solve_low_effort()
-        sol.profit_low_effort = self.expected_profit(sol.wH_loweffort, sol.wL_loweffort, 0)
+        # c. calculate profit when the principal wants low effort given monitoring decision
+        self.solve_low_effort(monitoring)
+        sol.profit_low_effort = self.expected_profit(sol.wH_loweffort,sol.wL_loweffort,0,monitoring)
 
+        # d. compare profit and store the solution with the highest profit
         if sol.profit_high_effort>sol.profit_low_effort:
             sol.wH = sol.wH_high_effort
             sol.wL = sol.wL_high_effort
@@ -160,33 +206,39 @@ class PrincipalAgentClass():
         else: 
             sol.wH = sol.wH_loweffort
             sol.wL = sol.wL_loweffort
-            sol.effort = True
+            sol.high_effort = True
             sol.profit = sol.profit_low_effort
+        
+        return sol.wH, sol.wL, sol.high_effort, sol.profit
 
-    def compare(self, parameter, low, high):
+    def compare(self, parameter,low,high,steps):
+        """ produce graphs with wages and profit depending on parameter values """
+        
+        # a. unpack
         par = self.par
         sol = self.sol
-        steps = par.steps
 
+        # b. producing grids and vectors to store the results
         grid = np.linspace(low,high,steps)
-
         wH = np.nan + np.zeros(steps)
         wL = np.nan + np.zeros(steps)
         profit_low_effort = np.nan + np.zeros(steps)
         profit_high_effort = np.nan + np.zeros(steps)
         
+        # c. solve the model for different parameter values
         for i, param_value in enumerate(grid):
-            par.__dict__[parameter] = param_value
+            setattr(par, parameter, param_value)
             self.solve()
             wH[i] = sol.wH
             wL[i] = sol.wL 
             profit_low_effort[i] = sol.profit_low_effort
             profit_high_effort[i] = sol.profit_high_effort
 
+        # d. plotting
         fig = plt.figure(figsize=(2*6,6/1.5))
 
         ax = fig.add_subplot(1,2,1)
-        ax.set_title(f'Wages depending on {str(parameter)}')
+        ax.set_title(f'Wages depending on {parameter}')
         ax.plot(grid,wH,label=r'$w_H$')
         ax.plot(grid,wL, label=r'$w_L$')
         ax.set(xlabel =f'{parameter}')
@@ -199,7 +251,3 @@ class PrincipalAgentClass():
         ax.set(xlabel =f'{parameter}')
         ax.legend(frameon=True)
         fig.tight_layout()
-
-
-
-    
